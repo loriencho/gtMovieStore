7ad2d217bf3931544afc3d216eef12244adce690
Dashjax <dashjax9@gmail.com>
2025-02-02 19:46:07 -0500

    Added better error handling and framework for custom signup and error screens.

--- accounts/views.py
+++ accounts/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
+from .forms import CustomUserCreationForm, CustomErrorList
 def signup(request):
     template_data = {}
     template_data['title'] = 'Sign Up'
     if request.method == 'GET':
-        template_data['form'] = UserCreationForm()
+        template_data['form'] = CustomUserCreationForm()
         return render(request, 'accounts/signup.html',
             {'template_data': template_data})
     elif request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
         if form.is_valid():
             form.save()
             return redirect('home.index')
