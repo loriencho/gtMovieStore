ce3d0fbc6f1892d448850eb191f384df04d06783
Dashjax <dashjax9@gmail.com>
2025-02-12 14:22:02 -0500

    Added option to clear cart.

--- cart/views.py
+++ cart/views.py
@@ -16,9 +16,14 @@ def index(request):
     template_data['movies_in_cart'] = movies_in_cart
     template_data['cart_total'] = cart_total
     return render(request, 'cart/index.html',{'template_data': template_data})
+
 def add(request, id):
     get_object_or_404(Movie, id=id)
     cart = request.session.get('cart', {})
     cart[id] = request.POST['quantity']
     request.session['cart'] = cart
+    return redirect('cart.index')
+
+def clear(request):
+    request.session['cart'] = {}
     return redirect('cart.index')
