f371d67b79293caf9ffae5b87edcec6ea02b0675
Dashjax <dashjax9@gmail.com>
2025-02-12 14:07:25 -0500

    Created cart app and added basic functionality to movie view.

--- gtMovieStore/urls.py
+++ gtMovieStore/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
 
 ]
 
